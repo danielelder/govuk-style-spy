b4c33cfea7ff1fca92139269b981620a10bfd2d7
Daniel Elder HMRC <daniel.elder@digital.hmrc.gov.uk>
2022-01-21 16:48:19 +0000

    updated activation and link positioning

--- background.js
+++ background.js
@@ -1,12 +1,15 @@
 chrome.runtime.onInstalled.addListener(function() {
-    chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-        chrome.declarativeContent.onPageChanged.addRules([{
-          conditions: [new chrome.declarativeContent.PageStateMatcher({
-            //pageUrl: {hostEquals: 'developer.chrome.com'},
-          })
-          ],
-              actions: [new chrome.declarativeContent.ShowPageAction()]
-        }]);
-      });
-
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+    chrome.declarativeContent.onPageChanged.addRules([{
+      conditions: [new chrome.declarativeContent.PageStateMatcher({
+        //pageUrl: {hostEquals: 'developer.chrome.com'},
+      })
+      ],
+      actions: [new chrome.declarativeContent.ShowPageAction()]
+    }]);
   });
+});
+
+chrome.browserAction.onClicked.addListener(function(tab) {
+  chrome.tabs.executeScript(null, {file: "styleSpy.js"});
+});
--- styleSpy.js
+++ styleSpy.js
@@ -1,96 +1,99 @@
 {
     const govCatalog = [
-        {"class":"govuk-accordion", "name":"Accordion", "component":"accordion"},
-        {"class":"govuk-back-link", "name":"Back link", "component":"back-link"},
-        {"class":"govuk-breadcrumbs", "name":"Breadcrumbs", "component":"breadcrumbs"},
-        {"class":"govuk-button", "name":"Button", "component":"button"},
-        {"class":"govuk-character-count", "name":"Character count", "component":"character-count"},
-        {"class":"govuk-checkboxes", "name":"Checkboxes", "component":"checkboxes"},
-        {"class":"govuk-cookie-banner", "name":"Cookie Banner", "component":"cookie-banner"},
-        {"class":"govuk-date-input", "name":"Date input", "component":"date-input"},
-        {"class":"govuk-details", "name":"Details", "component":"details"},
-        {"class":"govuk-form-group--error", "name":"Error message", "component":"error-message"},
-        {"class":"govuk-error-summary", "name":"Error summary", "component":"error-summary"},
-        {"class":"govuk-fieldset", "name":"Fieldset", "component":"fieldset"},
-        {"class":"govuk-file-upload", "name":"File upload", "component":"file-upload"},
-        {"class":"govuk-footer", "name":"Footer", "component":"footer"},
-        {"class":"govuk-header", "name":"Header", "component":"header"},
-        {"class":"govuk-inset-text", "name":"Inset text", "component":"inset-text"},
-        {"class":"govuk-notification-banner", "name":"Notification banner", "component":"notification-banner"},
-        {"class":"govuk-panel", "name":"Panel", "component":"panel"},
-        {"class":"govuk-phase-banner", "name":"Phase banner", "component":"phase-banner"},
-        {"class":"govuk-radios", "name":"Radios", "component":"radios"},
-        {"class":"govuk-select", "name":"Select", "component":"select"},
-        {"class":"govuk-skip-link", "name":"Skip link", "component":"skip-link"},
-        {"class":"govuk-summary-list", "name":"Summary list", "component":"summary-list"},
-        {"class":"govuk-table", "name":"Table", "component":"table"},
-        {"class":"govuk-tabs", "name":"Tabs", "component":"tabs"},
-        {"class":"govuk-tag", "name":"Tag", "component":"tag"},
-        {"class":"govuk-input", "name":"Text input", "component":"text-input"},
-        {"class":"govuk-textarea", "name":"Textarea", "component":"textarea"},
-        {"class":"govuk-warning-text", "name":"Warning text", "component":"warning-text"}
+        {"class":"govuk-accordion", "name":"Accordion", "component":"accordion", "label":"beforebegin"},
+        {"class":"govuk-back-link", "name":"Back link", "component":"back-link", "label":"beforebegin"},
+        {"class":"govuk-breadcrumbs", "name":"Breadcrumbs", "component":"breadcrumbs", "label":"beforebegin"},
+        {"class":"govuk-button", "name":"Button", "component":"button", "label":"afterend"},
+        {"class":"govuk-character-count", "name":"Character count", "component":"character-count", "label":"beforebegin"},
+        {"class":"govuk-checkboxes", "name":"Checkboxes", "component":"checkboxes", "label":"beforebegin"},
+        {"class":"govuk-cookie-banner", "name":"Cookie Banner", "component":"cookie-banner", "label":"beforebegin"},
+        {"class":"govuk-date-input", "name":"Date input", "component":"date-input", "label":"beforebegin"},
+        {"class":"govuk-details", "name":"Details", "component":"details", "label":"beforebegin"},
+        {"class":"govuk-form-group--error", "name":"Error message", "component":"error-message", "label":"beforebegin"},
+        {"class":"govuk-error-summary", "name":"Error summary", "component":"error-summary", "label":"beforebegin"},
+        {"class":"govuk-fieldset", "name":"Fieldset", "component":"fieldset", "label":"beforebegin"},
+        {"class":"govuk-file-upload", "name":"File upload", "component":"file-upload", "label":"beforebegin"},
+        {"class":"govuk-footer", "name":"Footer", "component":"footer", "label":"beforebegin"},
+        {"class":"govuk-header", "name":"Header", "component":"header", "label":"beforebegin"},
+        {"class":"govuk-inset-text", "name":"Inset text", "component":"inset-text", "label":"beforebegin"},
+        {"class":"govuk-notification-banner", "name":"Notification banner", "component":"notification-banner", "label":"beforebegin"},
+        {"class":"govuk-panel", "name":"Panel", "component":"panel", "label":"beforebegin"},
+        {"class":"govuk-phase-banner", "name":"Phase banner", "component":"phase-banner", "label":"beforebegin"},
+        {"class":"govuk-radios", "name":"Radios", "component":"radios", "label":"beforebegin"},
+        {"class":"govuk-select", "name":"Select", "component":"select", "label":"beforebegin"},
+        {"class":"govuk-skip-link", "name":"Skip link", "component":"skip-link", "label":"beforebegin"},
+        {"class":"govuk-summary-list", "name":"Summary list", "component":"summary-list", "label":"beforebegin"},
+        {"class":"govuk-table", "name":"Table", "component":"table", "label":"beforebegin"},
+        {"class":"govuk-tabs", "name":"Tabs", "component":"tabs", "label":"beforebegin"},
+        {"class":"govuk-tag", "name":"Tag", "component":"tag", "label":"beforebegin"},
+        {"class":"govuk-input", "name":"Text input", "component":"text-input", "label":"afterend"},
+        {"class":"govuk-textarea", "name":"Textarea", "component":"textarea", "label":"beforebegin"},
+        {"class":"govuk-warning-text", "name":"Warning text", "component":"warning-text", "label":"beforebegin"}
     ];
    
     const hmrcCatalog = [
-        {"class":"hmrc-header", "name":"Account header", "component":"account-header"},
-        {"class":"hmrc-account-menu", "name":"Account menu", "component":"account-header"},
-        {"class":"hmrc-list-with-actions", "name":"Add to list", "component":"add-to-a-list"},
-        {"class":"hmrc-banner", "name":"Banner", "component":"hmrc-banner"}, 
-        {"class":"hmrc-internal-header", "name":"Internal header", "component":"internal-header"}, 
-        {"class":"hmrc-notification-badge", "name":"Notification badge", "component":"notification-badge"}, 
-        {"class":"hmrc-page-heading", "name":"Page heading", "component":"page-heading"}, 
-        {"class":"hmrc-timeout-dialog", "name":"Service timeout", "component":"service-timeout"}, 
-        {"class":"hmrc-sign-out-nav", "name":"Sign out", "component":"sign-out"}, 
-        {"class":"hmrc-timeline", "name":"Timeline", "component":"timeline"}, 
-        {"class":"hmrc-language-select", "name":"Welsh language toggle", "component":"welsh-language-toggle"}  
+        {"class":"hmrc-header", "name":"Account header", "component":"account-header", "label":"afterend"},
+        {"class":"hmrc-account-menu", "name":"Account menu", "component":"account-header", "label":"beforebegin"},
+        {"class":"hmrc-list-with-actions", "name":"Add to list", "component":"add-to-a-list", "label":"beforebegin"},
+        {"class":"hmrc-banner", "name":"Banner", "component":"hmrc-banner", "label":"beforebegin"}, 
+        {"class":"hmrc-internal-header", "name":"Internal header", "component":"internal-header", "label":"afterend"}, 
+        {"class":"hmrc-notification-badge", "name":"Notification badge", "component":"notification-badge", "label":"beforebegin"}, 
+        {"class":"hmrc-page-heading", "name":"Page heading", "component":"page-heading", "label":"beforebegin"}, 
+        {"class":"hmrc-timeout-dialog", "name":"Service timeout", "component":"service-timeout", "label":"beforebegin"}, 
+        {"class":"hmrc-sign-out-nav", "name":"Sign out", "component":"sign-out", "label":"beforebegin"}, 
+        {"class":"hmrc-timeline", "name":"Timeline", "component":"timeline", "label":"beforebegin"}, 
+        {"class":"hmrc-language-select", "name":"Welsh language toggle", "component":"welsh-language-toggle", "label":"beforebegin"}  
     ];
 
     // Stylesheet
-    if (!document.getElementById('style-spy')) {
+    const spy = document.getElementById('style-spy');
+    if (!spy) {
         let style = document.createElement('style');
         style.innerHTML =
         `
         .stylespy-link {
-            color: black;
-            border: 1px solid black; 
-            background: white; 
+            color: #fff;
+            background: #A4235F;
+            border-bottom: 4px solid #A4235F;
+            display: inline-block;
             font-family: Arial,sans-serif;
-            font-size: 16px;
+            font-size: 14px;
             font-weight: normal;
-            padding: 10px 12px; 
-            position: absolute; 
-            width: 1px;
-            height: 1px;
-            margin: 0;
-            overflow: hidden;
-            clip: rect(0 0 0 0);
-            -webkit-clip-path: inset(50%);
-            clip-path: inset(50%);
+            outline: none;
+            padding: 8px 12px 4px 12px;
+            text-decoration: none;
         }
-        .stylespy-link--show,
+        .stylespy-link:hover {
+            text-decoration: underline;
+        }
         .stylespy-link:focus {
-            overflow: auto;
-            z-index: 99999999;
-            width: auto;
-            height: auto;
-            clip: auto;
-            -webkit-clip-path: none;
-            clip-path: none;
+            background-color: #fd0;
+            color: #0b0c0c;
+            border-color: #0b0c0c;
         }
         `;
         style.id = 'style-spy';
         document.head.appendChild(style);
+
+        scanPage('hmrc', hmrcCatalog, 'HMRC', 'https://design.tax.service.gov.uk/hmrc-design-patterns/');
+        scanPage('govuk', govCatalog, 'GOV.UK', 'https://design-system.service.gov.uk/components/');
+    }
+    else {
+        spy.remove();
+        document.querySelectorAll('.stylespy-link').forEach(function(link) {
+            link.remove();
+        });
+        document.querySelectorAll('[data-stylespy]').forEach(function(component) {
+            component.removeAttribute('style');
+            component.setAttribute('data-stylespy', 'none');
+        });
     }
 
-    scanPage('hmrc', hmrcCatalog, 'HMRC', 'https://design.tax.service.gov.uk/hmrc-design-patterns/');
-    scanPage('govuk', govCatalog, 'GOV.UK', 'https://design-system.service.gov.uk/components/');
-
     function scanPage(prefix, catalog, designSystem, designSystemURL) {
-        let elementCount = 0;
-
         for (let div of document.querySelectorAll('[class*=' + prefix + '-]')) { 
 
-            if (div.getAttribute('data-stylespy')) {
+            const dataStyleSpy = div.getAttribute('data-stylespy');
+            if (dataStyleSpy && (dataStyleSpy.value === 'govuk' || dataStyleSpy.value === 'hmrc')) {
                 continue;
             }
 
@@ -99,12 +102,8 @@
     
             if (result.length > 0) {
                 outlineComponent(div, result, prefix, designSystem, designSystemURL);
-
-                elementCount++;
             }
         }
-
-        console.log("Number of " + prefix + " definitions: ", elementCount);
     }
 
     function identifyElement(array, key, value) {
@@ -155,6 +154,6 @@
             link.classList.remove('stylespy-link--show');
         });
 
-        div.appendChild(link);
+        div.insertAdjacentHTML(result[0].label, link.outerHTML);
     }    
 }
